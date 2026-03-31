0a8abc1398381052b7d9174d4331f98dc8a3e057
Link4Electronics <link4electronics@gmail.com>
2026-03-31 01:32:30 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/audacious.svg
 export DESKTOP=/usr/share/applications/audacious.desktop
+export STARTUPWMCLASS=Audacious
 export DEPLOY_QT=1
 export DEPLOY_PIPEWIRE=1
 
