8ac2d663b273a4bd61253ce39111bcb3736590c5
domesya <renski.k2@gmail.com>
2026-09-02 19:29:31 +0200

    plugin test 1

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -19,3 +19,10 @@ pacman -Syu --noconfirm \
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
+
+# Build and install Audacious Discord RPC plugin
+echo "---- Building Audacious Discord RPC plugin... ----"
+git clone --depth 1 https://github.com/onegen-dev/audacious-discord-rpc.git /tmp/audacious-discord-rpc
+cmake -S /tmp/audacious-discord-rpc -B /tmp/audacious-discord-rpc/build -DCMAKE_BUILD_TYPE=Release
+cmake --build /tmp/audacious-discord-rpc/build -j$(nproc)
+install -Dm755 /tmp/audacious-discord-rpc/build/discord-rpc.so /usr/lib/audacious/General/discord-rpc.so
