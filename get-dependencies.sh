e05312e4fa39e8a5e9dcae43cfeccae2306566e6
domesya <renski.k2@gmail.com>
2026-09-02 19:44:39 +0200

    plugin test 3

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -22,9 +22,20 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
+
 # Build and install Audacious Discord RPC plugin
-echo "---- Building Audacious Discord RPC plugin... ----"
+echo "Building Audacious Discord RPC plugin..."
 git clone --depth 1 https://github.com/onegen-dev/audacious-discord-rpc.git /tmp/audacious-discord-rpc
+
+# Patch CMakeLists.txt: replace unconditional x86 flags with arch-conditional ones
+cd /tmp/audacious-discord-rpc
+if [ "$ARCH" = "aarch64" ]; then
+    sed -i '/-march=x86-64-v2/d' CMakeLists.txt
+    sed -i '/-mtune=generic/d' CMakeLists.txt
+    sed -i '/-fcf-protection=full/d' CMakeLists.txt
+fi
+cd
+
 cmake -S /tmp/audacious-discord-rpc -B /tmp/audacious-discord-rpc/build -DCMAKE_BUILD_TYPE=Release
 cmake --build /tmp/audacious-discord-rpc/build -j$(nproc)
 install -Dm755 /tmp/audacious-discord-rpc/build/discord-rpc.so /usr/lib/audacious/General/discord-rpc.so
